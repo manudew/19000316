b2e0fc737f3eb746c6141808d0ab7e63336007b9
manudew <84101425+manudew@users.noreply.github.com>
2021-05-14 17:36:36 +0530

    Add files via upload

---
+++ Convert.scala
@@ -0,0 +1,6 @@
+object Convert extends App{
+   def convert(x:Double): Double={
+      return x* 1.8 +32; 
+   }
+   println(convert(35)); 
+}
---
+++ Radius.scala
@@ -0,0 +1,6 @@
+object Radius extends App{
+  def radius(r:Double): Double={
+    return (4*22*r*r*r)/(3*7);
+  }
+  println("Volume 0f sphere : "+ radius(5));
+}
